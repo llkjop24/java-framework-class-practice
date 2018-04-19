192394a781c30339c40a7ba69fefa67706c77b7b
jiwon <llkjop24@gmail.com>
2018-04-20 02:54:46 +0900

    6. Connection 정보관리 Object를 DataSource로 변경

--- src/main/java/kr/ac/jejunu/DaoFactory.java
+++ src/main/java/kr/ac/jejunu/DaoFactory.java
@@ -1,18 +1,40 @@
 package kr.ac.jejunu;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Driver;
 
 @Configuration
 public class DaoFactory {
 
-    @Bean
-    public ProductDao productDao(){
-        return new ProductDao(connectionmaker());
-    }
+    @Value("${db.classname}")
+    private String className;
+    @Value("${db.url}")
+    private String url;
+    @Value("${db.username}")
+    private String username;
+    @Value("${db.password}")
+    private String password;
 
     @Bean
-    public ConnectionMaker connectionmaker(){
-        return new JejuConnectionMaker();
+    public ProductDao productDao(){
+        return new ProductDao(dataSource());
+    }
+
+    private DataSource dataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        try {
+            dataSource.setDriverClass((Class<? extends Driver>) Class.forName(className));
+        } catch (ClassNotFoundException e) {
+            new RuntimeException(e);
+        }
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
     }
 }
